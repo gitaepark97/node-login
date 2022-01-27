e468d2edbf2261192ec19666cc43f542bb1e414e
gitaepark <parkkitae7@gmail.com>
2022-01-27 13:19:43 +0900

    Create server with http

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
 
-app.get('/login', (req, res) => {
-  res.send('Login Page')
+// app.get('/login', (req, res) => {
+//   res.send('Login Page')
+// })
+
+// app.listen(8000, () => {
+//   console.log('Server with express is running')
+// })
+
+const http = require('http')
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }) // 한글 적용
+  if (req.url === '/') {
+    res.end('Hello World')
+  } else if (req.url === '/login') {
+    res.end('Login Page')
+  }
 })
 
 app.listen(8000, () => {
-  console.log('Server is running')
+  console.log('Server with http is running')
 })
