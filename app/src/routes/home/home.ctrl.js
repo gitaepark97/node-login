2150f686b801e55d4d8a27b325d08e512759db62
gitaepark <parkkitae7@gmail.com>
2022-01-31 17:33:43 +0900

    Login function in Backend

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
   },
 }
 
+const users = {
+  id: ['gitae97', 'harry', 'james'],
+  password: ['hgr30209!', '1234', '12345'],
+}
+
 const process = {
   login: (req, res) => {
-    console.log(req.body)
+    const id = req.body.id
+    const password = req.body.password
+
+    if (users.id.includes(id)) {
+      const index = users.id.indexOf(id)
+      if (users.password[index] === password) {
+        return res.json({
+          success: true,
+        })
+      }
+    }
+
+    return res.json({
+      success: false,
+      message: '로그인에 실패하셨습니다.',
+    })
   },
 }
 
