74a503c5eaea1614ee415ea9ee19197d82357ab8
gitaepark <parkkitae7@gmail.com>
2022-02-13 20:21:51 +0900

    Log management with Morgan&Winston

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const express = require('express')
-const app = express()
-
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+
+const app = express()
 dotenv.config()
+
+// const accessLogStream = require('./src/config/log')
 
 const home = require('./src/routes/home')
 
@@ -14,7 +17,8 @@ app.set('view engine', 'ejs')
 app.use(express.static(`${__dirname}/src/public`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+// app.use(morgan('dev'))
+// app.use(morgan('common', { stream: accessLogStream }))
 app.use('/', home)
 
 module.exports = app
